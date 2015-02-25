38dc2807b48a560aeda7cb952a40b373150667cb
Vladimir <web@libra.ms>
2015-02-25 19:31:32 +0300

    Updates

---
+++ components/com_kinoarhiv/models/releases.php
@@ -0,0 +1,236 @@
+<?php defined('_JEXEC') or die;
+/**
+ * @package     Kinoarhiv.Site
+ * @subpackage  com_kinoarhiv
+ *
+ * @copyright   Copyright (C) 2010 Libra.ms. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ * @url			http://киноархив.com/
+ */
+
+class KinoarhivModelReleases extends JModelList {
+	protected $context = null;
+
+	public function __construct($config = array()) {
+		parent::__construct($config);
+
+		if (empty($this->context)) {
+			$this->context = $this->context = strtolower('com_kinoarhiv.releases.global');
+		}
+	}
+
+	protected function getListQuery() {
+		$db = $this->getDBO();
+		$user = JFactory::getUser();
+		$groups	= implode(',', $user->getAuthorisedViewLevels());
+		$app = JFactory::getApplication();
+		$lang = JFactory::getLanguage();
+		$params = JComponentHelper::getParams('com_kinoarhiv');
+		$country = $app->input->get('country', '', 'word'); // It's a string because country_id == 0 - world premiere
+		$year = $app->input->get('year', 0, 'int');
+		$month = $app->input->get('month', '', 'string');
+
+		$query = $db->getQuery(true);
+
+		$query->select("`m`.`id`, `m`.`parent_id`, `m`.`title`, `m`.`alias`, `m`.`introtext` AS `text`, `m`.`plot`, `m`.`rate_loc`, `m`.`rate_sum_loc`, `m`.`imdb_votesum`, `m`.`imdb_votes`, `m`.`imdb_id`, `m`.`kp_votesum`, `m`.`kp_votes`, `m`.`kp_id`, `m`.`rottentm_id`, `m`.`rate_custom`, `m`.`year`, DATE_FORMAT(`m`.`created`, '%Y-%m-%d') AS `created`, DATE_FORMAT(`m`.`modified`, '%Y-%m-%d') AS `modified`, `m`.`created_by`, `m`.`attribs`, `m`.`state`, `g`.`filename`, `g`.`dimension`")
+			->from($db->quoteName('#__ka_movies').' AS `m`')
+			->leftJoin($db->quoteName('#__ka_movies_gallery').' AS `g` ON `g`.`movie_id` = `m`.`id` AND `g`.`type` = 2 AND `g`.`poster_frontpage` = 1 AND `g`.`state` = 1');
+
+			if ($country != '') {
+				$query->select(' `p`.`premiere_date`, `p`.`vendor_id`')
+				->leftJoin($db->quoteName('#__ka_premieres').' AS `p` ON `p`.`movie_id` = `m`.`id` AND `p`.`country_id` = (SELECT `id` FROM '.$db->quoteName('#__ka_countries').' WHERE `code` = "'.$db->escape($country).'" AND `language` IN ('.$db->quote($lang->getTag()).','.$db->quote('*').'))');
+
+				$query->select(' `v`.`company_name`, `v`.`company_name_intl`, `v`.`company_name_alias`')
+				->leftJoin($db->quoteName('#__ka_vendors').' AS `v` ON `v`.`id` = `p`.`vendor_id` AND `v`.`state` = 1');
+			} else {
+				$query->select(' `p`.`premiere_date`, `p`.`vendor_id`')
+				->leftJoin($db->quoteName('#__ka_premieres').' AS `p` ON `p`.`movie_id` = `m`.`id` AND `p`.`country_id` != 0');
+
+				$query->select(' `v`.`company_name`, `v`.`company_name_intl`, `v`.`company_name_alias`')
+				->leftJoin($db->quoteName('#__ka_vendors').' AS `v` ON `v`.`id` = `p`.`vendor_id` AND `v`.`state` = 1 AND `v`.`language` IN ('.$db->quote($lang->getTag()).','.$db->quote('*').')');
+			}
+
+		if (!$user->get('guest')) {
+			$query->select(' `u`.`favorite`')
+				->leftJoin($db->quoteName('#__ka_user_marked_movies').' AS `u` ON `u`.`uid` = '.$user->get('id').' AND `u`.`movie_id` = `m`.`id`');
+		}
+
+		$where = '`m`.`state` = 1 AND `m`.`language` IN ('.$db->quote($lang->getTag()).','.$db->quote('*').') AND `parent_id` = 0 AND `m`.`access` IN ('.$groups.')';
+
+		if ($params->get('use_alphabet') == 1) {
+			$letter = $app->input->get('letter', '', 'string');
+
+			if ($letter != '') {
+				if ($letter == '0-1') {
+					$where .= ' AND (`m`.`title` LIKE "0%" AND `m`.`title` LIKE "1%" AND `m`.`title` LIKE "2%" AND `m`.`title` LIKE "3%" AND `m`.`title` LIKE "4%" AND `m`.`title` LIKE "5%" AND `m`.`title` LIKE "6%" AND `m`.`title` LIKE "7%" AND `m`.`title` LIKE "8%" AND `m`.`title` LIKE "9%")';
+				} else {
+					preg_match('#\p{L}#u', $letter, $matches); // only any kind of letter from any language.
+
+					$where .= ' AND `m`.`title` LIKE "'.$db->escape(JString::strtoupper($matches[0])).'%"';
+				}
+			}
+		}
+
+		if ($country != '') {
+			$where .= ' AND `m`.`id` IN (SELECT `movie_id` FROM '.$db->quoteName('#__ka_premieres').' WHERE `country_id` = (SELECT `id` FROM '.$db->quoteName('#__ka_countries').' WHERE `code` = "'.$db->escape($country).'" AND `language` IN ('.$db->quote($lang->getTag()).','.$db->quote('*').')))';
+		}
+
+		if (!empty($year)) {
+			$where .= ' AND `m`.`id` IN (SELECT `movie_id` FROM '.$db->quoteName('#__ka_premieres').' WHERE `premiere_date` LIKE "%'.$year.'%")';
+		}
+
+		if ($month != '') {
+			$where .= ' AND `m`.`id` IN (SELECT `movie_id` FROM '.$db->quoteName('#__ka_premieres').' WHERE `premiere_date` LIKE "%'.$month.'%")';
+		}
+
+		$where .= " AND `p`.`premiere_date` != '".$db->nullDate()."'";
+
+		$query->where($where);
+		$query->group($db->quoteName('m.id'));
+
+		$orderCol = $this->state->get('list.ordering', $db->quoteName('p.premiere_date'));
+		$orderDirn = $this->state->get('list.direction', 'DESC');
+		$query->order($db->escape($orderCol.' '.$orderDirn));
+
+		return $query;
+	}
+
+	public function getSelectList() {
+		$db = $this->getDBO();
+		$app = JFactory::getApplication();
+		$lang = JFactory::getLanguage();
+		$country = $app->input->get('country', '', 'word'); // It's a string because country_id == 0 it'a world premiere
+		$year = $app->input->get('year', 0, 'int');
+		$month = $app->input->get('month', '', 'string');
+		$result = array(
+			'countries' => array(
+				array('name'=>JText::_('JALL'), 'code'=>'')
+			),
+			'years' => array(
+				array('value'=>0, 'name'=>JText::_('JALL'))
+			),
+			'months' => array(
+				array('value'=>'', 'name'=>JText::_('JALL'))
+			)
+		);
+
+		// Countries list
+		$db->setQuery("SELECT `name`, `code`"
+			. "\n FROM ".$db->quoteName('#__ka_countries')
+			. "\n WHERE `id` IN (SELECT `country_id` FROM ".$db->quoteName('#__ka_premieres')." WHERE `country_id` != 0) AND `state` = 1"
+			. "\n GROUP BY `code`");
+		try {
+			$countries = $db->loadAssocList();
+
+			if (count($countries) > 0) {
+				$result['countries'] = array_merge($result['countries'], $countries);
+			}
+		} catch (Exception $e) {
+			GlobalHelper::eventLog($e->getMessage());
+		}
+
+		// Years list
+		if ($country !== '') {
+			$year_where = " WHERE `country_id` = (SELECT `id` FROM ".$db->quoteName('#__ka_countries')." WHERE `code` = '".$db->escape($country)."' AND `language` IN (".$db->quote($lang->getTag()).",".$db->quote('*')."))";
+		} else {
+			$year_where = "";
+		}
+
+		$db->setQuery("SELECT DATE_FORMAT(`premiere_date`, '%Y') AS `value`, DATE_FORMAT(`premiere_date`, '%Y') AS `name`"
+			. "\n FROM ".$db->quoteName('#__ka_premieres')
+			. $year_where
+			. "\n GROUP BY `value`");
+		try {
+			$years = $db->loadAssocList();
+
+			if (count($years) > 0) {
+				$result['years'] = array_merge($result['years'], $years);
+			}
+		} catch (Exception $e) {
+			GlobalHelper::eventLog($e->getMessage());
+		}
+
+		// Months list
+		if ($country != '') {
+			$month_where = " WHERE `country_id` = (SELECT `id` FROM ".$db->quoteName('#__ka_countries')." WHERE `code` = '".$db->escape($country)."' AND `language` IN (".$db->quote($lang->getTag()).",".$db->quote('*')."))";
+
+			if (!empty($year)) {
+				$month_where .= " AND `premiere_date` LIKE '%".$year."%'";
+			}
+		} else {
+			if (!empty($year)) {
+				$month_where = " WHERE `premiere_date` LIKE '%".$year."%'";
+			} else {
+				$month_where = "";
+			}
+		}
+
+		$db->setQuery("SELECT DATE_FORMAT(`premiere_date`, '%Y-%m') AS `value`, `premiere_date`"
+			. "\n FROM ".$db->quoteName('#__ka_premieres')
+			. $month_where
+			. "\n GROUP BY `value`");
+		try {
+			$months = $db->loadAssocList();
+
+			if (count($months) > 0) {
+				foreach ($months as $key=>$month) {
+					$months[$key]['name'] = JHTML::_('date', strtotime($month['premiere_date']), 'F Y');
+				}
+
+				$result['months'] = array_merge($result['months'], $months);
+			}
+		} catch (Exception $e) {
+			GlobalHelper::eventLog($e->getMessage());
+		}
+
+		return $result;
+	}
+
+	public function getPagination() {
+		JLoader::register('KAPagination', JPATH_COMPONENT.DIRECTORY_SEPARATOR.'libraries'.DIRECTORY_SEPARATOR.'pagination.php');
+
+		$store = $this->getStoreId('getPagination');
+
+		if (isset($this->cache[$store])) {
+			return $this->cache[$store];
+		}
+
+		$limit = (int) $this->getState('list.limit') - (int) $this->getState('list.links');
+		$page = new KAPagination($this->getTotal(), $this->getStart(), $limit);
+
+		$this->cache[$store] = $page;
+
+		return $this->cache[$store];
+	}
+
+	protected function populateState($ordering = null, $direction = null) {
+		if ($this->context) {
+			$app = JFactory::getApplication();
+
+			$value = $app->getUserStateFromRequest($this->context . '.list.limit', 'limit', $app->get('list_limit'), 'uint');
+			$limit = $value;
+			$this->setState('list.limit', $limit);
+
+			$value = $app->getUserStateFromRequest($this->context . '.limitstart', 'limitstart', 0);
+			$limitstart = ($limit != 0 ? (floor($value / $limit) * $limit) : 0);
+			$this->setState('list.start', $limitstart);
+
+			$value = $app->getUserStateFromRequest($this->context . '.ordercol', 'filter_order', $ordering);
+			if (!in_array($value, $this->filter_fields)) {
+				$value = $ordering;
+				$app->setUserState($this->context . '.ordercol', $value);
+			}
+			$this->setState('list.ordering', $value);
+
+			$value = $app->getUserStateFromRequest($this->context . '.orderdirn', 'filter_order_Dir', $direction);
+			if (!in_array(strtoupper($value), array('ASC', 'DESC', ''))) {
+				$value = $direction;
+				$app->setUserState($this->context . '.orderdirn', $value);
+			}
+			$this->setState('list.direction', $value);
+		} else {
+			$this->setState('list.start', 0);
+			$this->state->set('list.limit', 0);
+		}
+	}
+}
---
+++ components/com_kinoarhiv/views/releases/view.html.php
@@ -0,0 +1,188 @@
+<?php defined('_JEXEC') or die;
+/**
+ * @package     Kinoarhiv.Site
+ * @subpackage  com_kinoarhiv
+ *
+ * @copyright   Copyright (C) 2010 Libra.ms. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ * @url			http://киноархив.com/
+ */
+
+class KinoarhivViewReleases extends JViewLegacy {
+	protected $items = null;
+	protected $pagination = null;
+	private $ka_theme = null;
+
+	public function display($tpl = null) {
+		$user = JFactory::getUser();
+		$app = JFactory::getApplication();
+		$lang = JFactory::getLanguage();
+
+		$items = $this->get('Items');
+		$list = $this->get('SelectList');
+		$pagination = $this->get('Pagination');
+
+		if (count($errors = $this->get('Errors'))) {
+			GlobalHelper::eventLog(implode("\n", $errors), 'ui');
+			return false;
+		}
+
+		$params = JComponentHelper::getParams('com_kinoarhiv');
+		$this->itemid = $app->input->get('Itemid', 0, 'int');
+		$this->ka_theme = $params->get('ka_theme');
+		$this->sel_country = $app->input->get('country', '', 'word');  // It's a string because country_id == 0 it'a world premiere
+		$this->sel_year = $app->input->get('year', 0, 'int');
+		$this->sel_month = $app->input->get('month', '', 'string');
+		$ka_theme = $params->get('ka_theme');
+		$itemid = $this->itemid;
+
+		// Prepare the data
+		foreach ($items as &$item) {
+			$item->attribs = json_decode($item->attribs);
+			$item->year_str = ($item->year != '0000') ? ' ('.$item->year.')' : '';
+			$item->vendor = $item->company_name;
+			if (!empty($item->company_name) && !empty($item->company_name_intl)) {
+				$item->vendor .= ' / ';
+			}
+			$item->vendor .= $item->company_name_intl;
+
+			// Replace country BB-code
+			$item->text = preg_replace_callback('#\[country\s+ln=(.+?)\](.*?)\[/country\]#i', function ($matches) use ($ka_theme) {
+				$html = JText::_($matches[1]);
+
+				$cn = preg_replace('#\[cn=(.+?)\](.+?)\[/cn\]#', '<img src="'.JURI::base().'components/com_kinoarhiv/assets/themes/component/'.$ka_theme.'/images/icons/countries/$1.png" border="0" alt="$2" class="ui-icon-country" /> $2', $matches[2]);
+
+				return $html.$cn;
+			}, $item->text);
+
+			// Replace genres BB-code
+			$item->text = preg_replace_callback('#\[genres\s+ln=(.+?)\](.*?)\[/genres\]#i', function ($matches) {
+				return JText::_($matches[1]).$matches[2];
+			}, $item->text);
+
+			// Replace person BB-code
+			$item->text = preg_replace_callback('#\[names\s+ln=(.+?)\](.*?)\[/names\]#i', function ($matches) use ($itemid) {
+				$html = JText::_($matches[1]);
+
+				$name = preg_replace('#\[name=(.+?)\](.+?)\[/name\]#', '<a href="'.JRoute::_('index.php?option=com_kinoarhiv&view=name&id=$1&Itemid='.$itemid, false).'" title="$2">$2</a>', $matches[2]);
+
+				return $html.$name;
+			}, $item->text);
+
+			if (empty($item->filename)) {
+				$item->poster = JURI::base().'components/com_kinoarhiv/assets/themes/component/'.$params->get('ka_theme').'/images/no_movie_cover.png';
+				$item->poster_width = 128;
+				$item->poster_height = 128;
+				$item->y_poster = '';
+			} else {
+				if (JString::substr($params->get('media_posters_root_www'), 0, 1) == '/') {
+					$item->big_poster = JURI::base().JString::substr($params->get('media_posters_root_www'), 1).'/'.JString::substr($item->alias, 0, 1).'/'.$item->id.'/posters/'.$item->filename;
+					$item->poster = JURI::base().JString::substr($params->get('media_posters_root_www'), 1).'/'.JString::substr($item->alias, 0, 1).'/'.$item->id.'/posters/thumb_'.$item->filename;
+				} else {
+					$item->big_poster = $params->get('media_posters_root_www').'/'.JString::substr($item->alias, 0, 1).'/'.$item->id.'/posters/'.$item->filename;
+					$item->poster = $params->get('media_posters_root_www').'/'.JString::substr($item->alias, 0, 1).'/'.$item->id.'/posters/thumb_'.$item->filename;
+				}
+				$item->poster_width = (int)$params->get('size_x_posters');
+				$orig_poster_size = explode('x', $item->dimension);
+				$item->poster_height = floor(($item->poster_width * $orig_poster_size[1]) / $orig_poster_size[0]);
+				$item->y_poster = ' y-poster';
+			}
+
+			$item->plot = JHtml::_('string.truncate', $item->plot, $params->get('limit_text'));
+
+			if ($params->get('ratings_show_frontpage') == 1) {
+				if (!empty($item->rate_sum_loc) && !empty($item->rate_loc)) {
+					$plural = $lang->getPluralSuffixes($item->rate_loc);
+					$item->rate_loc_c = round($item->rate_sum_loc / $item->rate_loc, (int)$params->get('vote_summ_precision'));
+					$item->rate_loc_label = JText::sprintf('COM_KA_RATE_LOCAL_'.$plural[0], $item->rate_loc_c, (int)$params->get('vote_summ_num'), $item->rate_loc);
+					$item->rate_loc_label_class = ' has-rating';
+				} else {
+					$item->rate_loc_c = 0;
+					$item->rate_loc_label = '<br />'.JText::_('COM_KA_RATE_NO');
+					$item->rate_loc_label_class = ' no-rating';
+				}
+			}
+
+			$item->event = new stdClass;
+			$item->params = new JObject;
+			$item->params->set('url', JRoute::_('index.php?option=com_kinoarhiv&view=movie&id='.$item->id.'&Itemid='.$this->itemid), false);
+
+			$dispatcher = JEventDispatcher::getInstance();
+			JPluginHelper::importPlugin('content');
+			$dispatcher->trigger('onContentPrepare', array('com_kinoarhiv.premieres', &$item, &$params, 0));
+
+			$results = $dispatcher->trigger('onContentAfterTitle', array('com_kinoarhiv.premieres', &$item, &$item->params, 0));
+			$item->event->afterDisplayTitle = trim(implode("\n", $results));
+
+			$results = $dispatcher->trigger('onContentBeforeDisplay', array('com_kinoarhiv.premieres', &$item, &$item->params, 0));
+			$item->event->beforeDisplayContent = trim(implode("\n", $results));
+
+			$results = $dispatcher->trigger('onContentAfterDisplay', array('com_kinoarhiv.premieres', &$item, &$item->params, 0));
+			$item->event->afterDisplayContent = trim(implode("\n", $results));
+		}
+
+		$this->params = &$params;
+		$this->items = &$items;
+		$this->selectlist = &$list;
+		$this->pagination = &$pagination;
+		$this->user = &$user;
+
+		$this->_prepareDocument();
+
+		parent::display($tpl);
+	}
+
+	/**
+	 * Prepares the document
+	 */
+	protected function _prepareDocument() {
+		$app = JFactory::getApplication();
+		$menus = $app->getMenu();
+		$menu = $menus->getActive();
+		$pathway = $app->getPathway();
+		$title = ($menu && $menu->link != 'index.php?option=com_kinoarhiv&view=releases') ? JText::_('COM_KA_RELEASES') : $menu->title;
+
+		// Create a new pathway object
+		$path = (object)array(
+			'name' => $title,
+			'link' => 'index.php?option=com_kinoarhiv&view=releases&Itemid='.$this->itemid
+		);
+
+		$pathway->setPathway(array($path));
+		$this->document->setTitle($title);
+
+		if ($menu->params->get('menu-meta_description') != '') {
+			$this->document->setDescription($menu->params->get('menu-meta_description'));
+		} else {
+			$this->document->setDescription($this->params->get('meta_description'));
+		}
+
+		if ($menu->params->get('menu-meta_keywords') != '') {
+			$this->document->setMetadata('keywords', $menu->params->get('menu-meta_keywords'));
+		} else {
+			$this->document->setMetadata('keywords', $this->params->get('meta_keywords'));
+		}
+
+		if ($menu->params->get('robots') != '') {
+			$this->document->setMetadata('robots', $menu->params->get('robots'));
+		} else {
+			$this->document->setMetadata('robots', $this->params->get('robots'));
+		}
+
+		if ($this->params->get('generator') == 'none') {
+			$this->document->setGenerator('');
+		} elseif ($this->params->get('generator') == 'site') {
+			$this->document->setGenerator($this->document->getGenerator());
+		} else {
+			$this->document->setGenerator($this->params->get('generator'));
+		}
+
+		if ($this->params->get('show_feed_link', 1)) {
+			$link = 'index.php?option=com_kinoarhiv&view=movies&format=feed&Itemid='.$this->itemid.'&limitstart=';
+			$attribs = array('type' => 'application/rss+xml', 'title' => 'RSS 2.0');
+			$this->document->addHeadLink(JRoute::_($link.'&type=rss'), 'alternate', 'rel', $attribs);
+			$attribs = array('type' => 'application/atom+xml', 'title' => 'Atom 1.0');
+			$this->document->addHeadLink(JRoute::_($link.'&type=atom'), 'alternate', 'rel', $attribs);
+		}
+	}
+}
